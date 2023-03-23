703ebead5f94893e79d35a06dc2ce0710ec9b229
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-03-23 10:32:43 +0700

    chore(linters): Настроить eslint, stylelint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  "plugins": [
+    "react-hooks",
+    "@typescript-eslint",
+    "prettier"
+  ],
+  "overrides": [],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+    "camelcase": "error",
+    "spaced-comment": "error",
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "no-duplicate-imports": "error"
+  }
+}

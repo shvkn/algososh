efceaa318b4259a1db30c11423d46ba2c08e2e43
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-04-21 23:55:33 +0700

    test(sort): Покрыть unit тестами алгоритм сортировки массива

--- src/components/sorting-page/sorting.test.tsx
+++ src/components/sorting-page/sorting.test.tsx
@@ -2,63 +2,78 @@ import { renderHook } from "@testing-library/react-hooks";
 import { act } from "react-test-renderer";
 
 import { SortingAlgorithm, useAnimatedSorting } from "./utils";
+import { Direction } from "../../types";
 
-describe("Sorting bubble", () => {
-  it("Sorting []", async () => {
+describe("Bubble sort algorithm", () => {
+  it("Should works with an empty array", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [];
     let sortedAdd: number[] = [];
 
     await act(async () => {
-      result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
+      await result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
       sortedAdd = await result.current.sort(arr);
     });
 
     expect(sortedAdd).toEqual(arr);
   });
 
-  it("Sorting [1]", async () => {
+  it("Should works with an array of single element", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [1];
     let sortedAdd: number[] = [];
 
     await act(async () => {
-      result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
+      await result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
       sortedAdd = await result.current.sort(arr);
     });
 
     expect(sortedAdd).toEqual(arr);
   });
 
-  it("Sorting [43, 2, 21, 435]", async () => {
+  it("Should sort an array ascending", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [43, 2, 21, 435];
     let sortedAdd: number[] = [];
 
     await act(async () => {
-      result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
+      await result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
       sortedAdd = await result.current.sort(arr);
     });
 
     expect(sortedAdd).toEqual(arr.sort());
+  });
+
+  it("Should sort an array descending", async () => {
+    const { result } = renderHook(() => useAnimatedSorting(0));
+    const arr: number[] = [43, 2, 21, 435];
+    let sortedAdd: number[] = [];
+
+    await act(async () => {
+      await result.current.setAlgorithm(SortingAlgorithm.BubbleSort);
+      await result.current.setDirection(Direction.Descending);
+      sortedAdd = await result.current.sort(arr);
+    });
+
+    expect(sortedAdd).toEqual(arr.sort((a, b) => b - a));
   });
 });
 
-describe("Sorting selection", () => {
-  it("Sorting []", async () => {
+describe("Selection sort algorithm", () => {
+  it("Should works with an empty array", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [];
     let sortedAdd: number[] = [];
 
     await act(async () => {
-      result.current.setAlgorithm(SortingAlgorithm.SelectionSort);
+      await result.current.setAlgorithm(SortingAlgorithm.SelectionSort);
       sortedAdd = await result.current.sort(arr);
     });
 
     expect(sortedAdd).toEqual(arr);
   });
 
-  it("Sorting [1]", async () => {
+  it("Should works with an array of single element", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [1];
     let sortedAdd: number[] = [];
@@ -71,16 +86,30 @@ describe("Sorting selection", () => {
     expect(sortedAdd).toEqual(arr);
   });
 
-  it("Sorting [43, 2, 21, 435]", async () => {
+  it("Should sort an array ascending", async () => {
     const { result } = renderHook(() => useAnimatedSorting(0));
     const arr: number[] = [43, 2, 21, 435];
     let sortedAdd: number[] = [];
 
     await act(async () => {
-      result.current.setAlgorithm(SortingAlgorithm.SelectionSort);
+      await result.current.setAlgorithm(SortingAlgorithm.SelectionSort);
       sortedAdd = await result.current.sort(arr);
     });
 
     expect(sortedAdd).toEqual(arr.sort());
+  });
+
+  it("Should sort an array descending", async () => {
+    const { result } = renderHook(() => useAnimatedSorting(0));
+    const arr: number[] = [43, 2, 21, 435];
+    let sortedAdd: number[] = [];
+
+    await act(async () => {
+      await result.current.setAlgorithm(SortingAlgorithm.SelectionSort);
+      await result.current.setDirection(Direction.Descending);
+      sortedAdd = await result.current.sort(arr);
+    });
+
+    expect(sortedAdd).toEqual(arr.sort((a, b) => b - a));
   });
 });
